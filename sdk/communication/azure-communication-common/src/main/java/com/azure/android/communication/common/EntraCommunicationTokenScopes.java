b8b2acdbc07e289e5cc85fb9525a3518fdbb17af
Alexandra Lamiova Pistrakova <apistrak@microsoft.com>
2025-05-30 14:06:12 +0200

    Rename parameters

--- sdk/communication/azure-communication-common/src/main/java/com/azure/android/communication/common/EntraCommunicationTokenScopes.java
+++ sdk/communication/azure-communication-common/src/main/java/com/azure/android/communication/common/EntraCommunicationTokenScopes.java
@@ -6,8 +6,8 @@ package com.azure.android.communication.common;
  * The Entra Communication Token scopes.
  */
 public class EntraCommunicationTokenScopes {
-    public static final String TEAMSEXTENSIONSCOPEPREFIX = "https://auth.msft.communication.azure.com/";
-    public static final String COMMUNICATIONCLIENTSSCOPEPREFIX = "https://communication.azure.com/clients/";
-    public static final String DEFAULTSCOPES = COMMUNICATIONCLIENTSSCOPEPREFIX + ".default";
+    public static final String TEAMS_EXTENSION_SCOPE_PREFIX = "https://auth.msft.communication.azure.com/";
+    public static final String COMMUNICATION_CLIENTS_SCOPE_PREFIX = "https://communication.azure.com/clients/";
+    public static final String DEFAULT_SCOPES = COMMUNICATION_CLIENTS_SCOPE_PREFIX + ".default";
 }
 
