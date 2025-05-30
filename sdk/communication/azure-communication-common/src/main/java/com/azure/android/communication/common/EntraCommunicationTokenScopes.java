d6c181dc6817d822b79455e0160f7a336eb5d274
Alexandra Lamiova Pistrakova <apistrak@microsoft.com>
2025-05-30 14:06:12 +0200

    Add EntraCommunicationTokenScopes class

---
+++ sdk/communication/azure-communication-common/src/main/java/com/azure/android/communication/common/EntraCommunicationTokenScopes.java
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+package com.azure.android.communication.common;
+
+/**
+ * The Entra Communication Token scopes.
+ */
+public class EntraCommunicationTokenScopes {
+    public static final String TEAMSEXTENSIONSCOPEPREFIX = "https://auth.msft.communication.azure.com/";
+    public static final String COMMUNICATIONCLIENTSSCOPEPREFIX = "https://communication.azure.com/clients/";
+    public static final String DEFAULTSCOPES = COMMUNICATIONCLIENTSSCOPEPREFIX + ".default";
+}
+
