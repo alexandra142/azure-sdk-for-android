3572eed14551766a8a1594e96739abe5425fbc20
Alexandra Lamiova Pistrakova <apistrak@microsoft.com>
2025-06-02 14:25:17 +0200

    Prepare EntraTokenCredential

--- sdk/communication/azure-communication-common/src/main/java/com/azure/android/communication/common/EntraCommunicationTokenScopes.java
+++ sdk/communication/azure-communication-common/src/main/java/com/azure/android/communication/common/EntraCommunicationTokenScopes.java
@@ -5,7 +5,7 @@ package com.azure.android.communication.common;
 /**
  * The Entra Communication Token scopes.
  */
-public class EntraCommunicationTokenScopes {
+class EntraCommunicationTokenScopes {
     public static final String TEAMS_EXTENSION_SCOPE_PREFIX = "https://auth.msft.communication.azure.com/";
     public static final String COMMUNICATION_CLIENTS_SCOPE_PREFIX = "https://communication.azure.com/clients/";
     public static final String DEFAULT_SCOPES = COMMUNICATION_CLIENTS_SCOPE_PREFIX + ".default";
