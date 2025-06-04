95aeddc627470c6b6b49d3ac9141df3ea5832577
Alexandra Lamiova Pistrakova <apistrak@microsoft.com>
2025-06-04 18:45:52 +0200

    Remove EntraTokenGuardPolicy as there is some issue not relevant for adding Azure.Identity library

---
+++ sdk/communication/azure-communication-common/src/test/java/com/azure/android/communication/common/EntraCommunicationTokenCredentialOptionsTest.java
@@ -0,0 +1,11 @@
+package com.azure.android.communication.common;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+import org.junit.jupiter.api.Test;
+
+public class EntraCommunicationTokenCredentialOptionsTest extends TokenCredentialBaseTest {
+       @Test
+       public void GetCredentials() {
+           assertNull(null);
+   }
+}
