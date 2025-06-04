6f2d4413deda2e612440f00e380efe4b9bd17033
Alexandra Lamiova Pistrakova <apistrak@microsoft.com>
2025-06-04 18:53:55 +0200

    Add Azure.Identity and try to test it.

--- sdk/communication/azure-communication-common/src/test/java/com/azure/android/communication/common/EntraCommunicationTokenCredentialOptionsTest.java
+++ sdk/communication/azure-communication-common/src/test/java/com/azure/android/communication/common/EntraCommunicationTokenCredentialOptionsTest.java
@@ -1,11 +1,31 @@
 package com.azure.android.communication.common;
 
 import static org.junit.jupiter.api.Assertions.assertNull;
+
+import com.azure.android.core.credential.TokenCredential;
+import com.azure.identity.InteractiveBrowserCredential;
+import com.azure.identity.InteractiveBrowserCredentialBuilder;
+
 import org.junit.jupiter.api.Test;
 
 public class EntraCommunicationTokenCredentialOptionsTest extends TokenCredentialBaseTest {
-       @Test
+
+
+    @Test
        public void GetCredentials() {
+
+           InteractiveBrowserCredential tokenCredential = new InteractiveBrowserCredentialBuilder()
+               .clientId("cd17d9da-3d0d-4357-bb9c-3a1d948636ce")
+               .tenantId("be5c2424-1562-4d62-8d98-815720d06e4a")
+               .redirectUrl("http://localhost:8080")
+               .build();
+
+           String resourceEndpoint = "https://<your-resource>.communication.azure.com";
+           String[] scopes = new String[] { "https://auth.msft.communication.azure.com/TeamsExtension.ManageCalls" };
+
+          EntraCommunicationTokenCredentialOptions entraTokenCredentialOptions = new EntraCommunicationTokenCredentialOptions(resourceEndpoint, (TokenCredential) tokenCredential, scopes);
+         //  CommunicationTokenCredential credential = new CommunicationTokenCredential(entraTokenCredentialOptions);
+
            assertNull(null);
    }
 }
